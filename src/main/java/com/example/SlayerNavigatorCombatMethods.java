b6c183b5ea782947fea1fecb1a0812fb97f0b542
Matthias van den Belt <mvandenbelt@bio-prodict.nl>
2022-06-25 14:06:09 +0200

    add descriptors at combat methods

--- src/main/java/com/example/SlayerNavigatorCombatMethods.java
+++ src/main/java/com/example/SlayerNavigatorCombatMethods.java
@@ -1,15 +1,23 @@
 package net.runelite.client.plugins.slayernavigator;
 
+
 public enum SlayerNavigatorCombatMethods {
-    MELEE_PRAYER,
-    MELEE_DEFENCE,
-    MAGIC_BURST,
-    MAGIC_SAFESPOT,
-    RANGED_SAFESPOT
+    MELEE_PRAYER("Melee - Prayer"),
+    MELEE_DEFENCE("Melee - Defence"),
+    MAGIC_BURST("Magic - Burst"),
+    MAGIC_SAFESPOT("Magic - Safespot"),
+    RANGED_SAFESPOT("Ranged - Safespot");
 //
 //    * Melee - Prayer
 //    * Melee - Defence
 //    * Magic - Burst
 //    * Magic - Safespot
 //    * Ranged - Safespot
+
+    private final String name;
+
+    SlayerNavigatorCombatMethods(String name) {
+        this.name = name;
+    }
+
 }
